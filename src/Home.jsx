062870e057afbcd38691abbf0d28a0cde9fa4e7f
ajita-dubey <dubeyajita305@gmail.com>
2025-07-16 21:03:00 +0530

    project completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,28 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Home = () => {
 const [data, setData] = useState([])
+const navigate = useNavigate();
+
   console.log(data)
   useEffect(() => {
     axios.get('http://localhost:3000/users')
     .then(res => setData(res.data))
     .catch(err => console.log(err));
-  },[])  
+  },[]) 
+  
+  const handleDelete = (id) => {
+    const confirm = window.confirm("Would you like to Delete");
+    if(confirm) {
+      axios.delete('http://localhost:3000/users/' +id)
+      .then(res =>{
+        location.reload();
+      })
+      .catch(err => console.log(err));
+    }
+  }
   return (
     <div className='d-flex  flex-column justify-content-center align-items-center bg-light vh-100'>
       <h1>List of Users</h1>
@@ -31,14 +44,14 @@ const [data, setData] = useState([])
             {
              data.map((d, i) => (
                 <tr key={i}>
-                 <td>{d.id}</td>
+                 <td>{i+1}</td>
                  <td>{d.name}</td>
                  <td>{d.email}</td>
                  <td>{d.phone}</td>
                  <td>
                    <Link to={`/read/${d.id}`} className='btn btn-sm btn-info me-2'>Read</Link>
                   <Link className='btn btn-sm btn-primary me-2' to={`/update/${d.id}`}>Edit</Link>
-                  <button className='btn btn-sm btn-danger'>Delete</button>
+                  <button onClick={ e => handleDelete(d.id)} className='btn btn-sm btn-danger'>Delete</button>
                  </td>
 
               </tr>
@@ -51,6 +64,7 @@ const [data, setData] = useState([])
       
     </div>
   )
+  
 }
 
 export default Home
